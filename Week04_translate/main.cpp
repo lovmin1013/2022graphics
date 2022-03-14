cb35e97008edadefe31daf0889b0c0dd944e0205
lovmin1013 <jenny.tsai32@gmail.com>
2022-03-14 16:41:36 +0800

    Week04

---
+++ Week04_translate/main.cpp
@@ -0,0 +1,27 @@
+#include <GL/glut.h>
+void myTeapot(float x, float y) ///自訂函式
+{
+    glPushMatrix();///備份陣列
+        glTranslatef(x, y, 0);
+        glutSolidTeapot(0.3);
+    glPopMatrix();///還原矩陣
+}
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glColor3f(1, 1, 0); ///黃色
+    myTeapot(+0.5, +0.5);
+    myTeapot(-0.5, +0.5);
+    myTeapot(-0.5, -0.5);
+    myTeapot(+0.5, -0.5);
+    glutSwapBuffers();
+}
+
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("Translate");
+    glutDisplayFunc(display);
+    glutMainLoop();
+}
